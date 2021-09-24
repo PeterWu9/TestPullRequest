8a53a21c4cf6e61b0ac5ff89c72f5ca3154f60fb
Peter Wu <petey888@gmail.com>
2021-09-23 18:38:52 -0700

    Initial Commit

---
+++ TestPullRequest/TestPullRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPullRequestApp.swift
+//  TestPullRequest
+//
+//  Created by Peter Wu on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct TestPullRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
